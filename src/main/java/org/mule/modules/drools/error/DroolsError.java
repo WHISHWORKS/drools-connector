bb136439fbd87938c2c9c3850fb9d01c832b460b
kishan.soni <kishan.soni@whishworks.com>
2016-09-28 12:48:25 +0000

    Kishan: change for exception handling

---
+++ src/main/java/org/mule/modules/drools/error/DroolsError.java
@@ -0,0 +1,12 @@
+package org.mule.modules.drools.error;
+
+public class DroolsError extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	public DroolsError() {
+	}
+	public DroolsError(String message) {
+		 super(message);
+	}
+
+}
