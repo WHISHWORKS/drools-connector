d092a5d234c0b420c5d9264adf182110deb30e87
kishan.soni@whishworks.com <kishan.soni@whishworks.com@ec9ada9f-12b0-4efb-a67a-e677e2a7ec32>
2016-09-28 12:51:06 +0000

    Kishan: checked in target folder

--- trunk/drools_connector/drools-connector3.8/drools-connector/target/generated-sources/mule/org/mule/modules/drools/generated/config/DroolsNamespaceHandler.java
+++ trunk/drools_connector/drools-connector3.8/drools-connector/target/generated-sources/mule/org/mule/modules/drools/generated/config/DroolsNamespaceHandler.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
  * 
  */
 @SuppressWarnings("all")
-@Generated(value = "Mule DevKit Version 3.9.0", date = "2016-09-21T12:29:12+05:30", comments = "Build UNNAMED.2793.f49b6c7")
+@Generated(value = "Mule DevKit Version 3.9.0", date = "2016-09-28T05:03:46+05:30", comments = "Build UNNAMED.2793.f49b6c7")
 public class DroolsNamespaceHandler
     extends NamespaceHandlerSupport
 {
